7a908dc90df82e2a0238a2cd4e7c2ad8926b9420
godpelden <peldenastra@gmail.com>
2026-05-22 17:01:09 +0600

    Add files via upload

---
+++ functional.js
@@ -0,0 +1,17 @@
+function openModal(){
+document.getElementById('donationModal').style.display = 'block';
+}
+
+function closeModal(){
+document.getElementById('donationModal').style.display = 'none';
+}
+
+window.onclick = function(event){
+const modal = document.getElementById('donationModal');
+if(event.target == modal){
+modal.style.display = 'none';
+}
+}
+
+
+
